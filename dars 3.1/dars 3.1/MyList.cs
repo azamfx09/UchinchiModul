86b1cd94143e5b8f87934c920f6d6a8e7bb2f5d0
azamfx09 <dd1778673@gmail.com>
2026-01-31 18:03:41 +0500

    done

---
+++ dars 3.1/dars 3.1/MyList.cs
@@ -0,0 +1,106 @@
+﻿namespace dars_3._1;
+
+public class MyList : IMyList
+{
+    private int[] _nums;
+    private int arrIndex = 0;
+    public int Capacity
+    {
+        get { return _nums.Length; }
+    }
+
+    public MyList(int capacity = 4)
+    {
+        _nums = new int[capacity];
+    }
+    public bool Add(int num)
+    {
+        if (arrIndex >= Capacity)
+        {
+            DoubleCapacity();
+        }
+
+        _nums[arrIndex] = num;
+        arrIndex++;
+
+        return true;
+    }
+
+    private void DoubleCapacity()
+    {
+        int[] newArr = new int[Capacity * 2];
+        for (var i = 0; i < Capacity; i++)
+        {
+            newArr[i] = _nums[i];
+        }
+        _nums = newArr;
+    }
+
+    public bool Contains(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetById(int index)
+    {
+        return _nums[index];
+    }
+
+    public int IndexOf(int num)
+    {
+        for (var i = 0; i < Capacity; i++)
+        {
+            if (_nums[i] == num)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public bool Remove(int num)
+    {
+        for (var i = 0; i < Capacity; i++)
+        {
+            if (_nums[i] == num)
+            {
+                for (var j = i; j < Capacity - 1; j++)
+                {
+                    _nums[j] = _nums[j + 1];
+                }
+                --arrIndex;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool RemoveAll(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DisplayElements()
+    {
+        foreach (var i in _nums)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    public bool RemoveAt (int index)
+    {
+        for (var i = 0; i <= Capacity; i++)
+        {
+
+        }
+    }
+}
+
--- dars 3.1/dars 3.1/Program.cs
+++ dars 3.1/dars 3.1/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace dars_3._1
+﻿namespace dars_3._1;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        MyList myList = new MyList(3);
+        myList.Add(77);
+        myList.Add(71);
+        myList.Add(73);
+        Console.WriteLine(myList.Capacity);
+        myList.Remove(71);
+        myList.Add(11);
+        Console.WriteLine(myList.Capacity);
+        myList.DisplayElements();
     }
 }
